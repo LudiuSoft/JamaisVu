00c15d309aef6796c12f2dd42421d7e1ff7c58e6
ChemiCalChems <txemaynzenga@gmail.com>
2016-03-18 23:55:11 +0100

    Testing Win and Linux Qt lib compatibility

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <qt_windows.h>
+#include <qt4/Qt/q3accel.h>
 
 using namespace std;
 
 int main() {
     cout << "fagit" << endl;
-    CS_DROPSHADOW;
     return 0;
 }
