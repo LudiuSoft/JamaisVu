b52b0e49f3d83d28e9801b570cb1bd7c4d627853
ChemiCalChems <txemaynzenga@gmail.com>
2016-03-19 09:12:08 +0100

    Solve pull conflicts 2

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
-<<<<<<< HEAD
 #include "wx/wx.h"
-=======
->>>>>>> 41398c1867429e60bfc7449040b396ca2ca3a860
 
 using namespace std;
 
 int main() {
-<<<<<<< HEAD
     wxApp app;
     app.CheckSignal();
     cout << "fagit" << endl;
-=======
-    cout << "fagit" << endl;
->>>>>>> 41398c1867429e60bfc7449040b396ca2ca3a860
     return 0;
 }
