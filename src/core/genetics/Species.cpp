81deafee9c3af99b64a15a792b3215d4e0b5ae5c
ThisIsIPat <ThisIsIPat@users.noreply.github.com>
2016-03-29 17:06:38 +0200

    Fixing potential bugs regarding the fitness function and pointing at other possible bugs

--- src/core/genetics/Species.cpp
+++ src/core/genetics/Species.cpp
@@ -11,10 +11,26 @@ Species::Species(std::string _name)
 
 double Species::getAverageFitness()
 {
-    int totalFitness;
-    for (int i = 0; i<=genomes.size(); i++)
+    int totalFitness = 0;
+
+    /* TODO
+     * LINE a1:
+     *
+     * In java, size() would return the amount of elements. If that's the case, we'd have a
+     * random memory access at the LINE a2 when it reaches the point beyond the last element,
+     * because of the <= operator in a1. Change it to < if I'm right, I didn't do it since I'm unsure...
+     *
+     * EXAMPLE: Vector ( 0, 1, 2 ) has 3 elements
+     * size() = 3;              This line is what I'm unsure about
+     * i=0 <= size() : true
+     * i=1 <= size() : true
+     * i=2 <= size() : true
+     * i=3 <= size() : TRUE     (should be false, will return random memory)
+     */
+
+    for (unsigned int i = 0; i<=genomes.size(); i++)    // a1
     {
-        totalFitness += genomes.at(i).fitness;
+        totalFitness += genomes.at(i).fitness;          // a2
     }
     return totalFitness/genomes.size();
 }
